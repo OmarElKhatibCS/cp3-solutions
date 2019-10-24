21f1c627899feb0303b4f05c771145ee2ab1d41a
OmarElKhatibCS <omarelkhatib@mail.com>
2019-10-24 18:09:56 +0300

    solve uva12250

---
+++ 1 - Introduction/1 - Getting Started: The Easy Problems/1 - Super Easy/uva12250.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main() {
+  int TC;
+  char s[8];
+  scanf("%d" , &TC);
+  while(TC--) {
+    scanf("%s",&s);
+    if(strcmp(s,"#") == 0 ) break;
+    if(strcmp(s , "HELLO") ==0 ) printf("ENGLISH\n");
+    else if(strcmp(s,"HOLA")==0) printf("SPANISH\n");
+    else if(strcmp(s,"HALLO")==0) printf("GERMAN\n");
+    else if(strcmp(s,"BONJOUR")==0) printf("FRENCH\n");
+    else if(strcmp(s,"CIAO")==0) printf("ITALIAN\n");
+    else if(strcmp(s,"ZDRAVSTVUJTE")==0) printf("RUSSIAN\n");
+    else printf("UNKNOWN\n");
+  }
+  return 0;
+}
