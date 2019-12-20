db77c111a9da7a315f47fe3b3d061c5799cef7c9
OmarElKhatibCS <omarelkhatib@mail.com>
2019-12-21 01:52:29 +0200

    solve uva162

--- 1 - Introduction/2 - Ad Hoc Problems/01 - Game (Card)/uva00162.cpp
+++ 1 - Introduction/2 - Ad Hoc Problems/01 - Game (Card)/uva00162.cpp
@@ -1,85 +1,123 @@
+#include <cstdio>
+#include <deque>
 #include <iostream>
-#include <vector>
 #include <string>
-#include <map>
-
 using namespace std;
 
-template<typename K, typename V>
-void print_map(map<K,V> const &m)
-{
-    for (auto const& pair: m) {
-        cout << "{" << pair.first << ": " << pair.second << "}\n";
-    }
+int card2int(const string &card) {
+  switch (card[1]) {
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      return int(card[1] - '0');
+      break;
+    case 'T':
+      return 10;
+      break;
+    case 'J':
+      return 11;
+      break;
+    case 'Q':
+      return 12;
+      break;
+    case 'K':
+      return 13;
+      break;
+    case 'A':
+      return 14;
+      break;
+  }
+  return -1;
 }
 
-int main() {
-	map<string , int> cards;
-	// Diamonds
-	for(int i=2;i<=9;i++)
-		cards.insert(pair <string , int>  ("D"+to_string(2) , i-2));
-	cards.insert(pair <string , int>  ("DT"  , 8));
-	cards.insert(pair <string , int>  ("DJ" , 9));
-	cards.insert(pair <string , int>  ("DQ" , 10));
-	cards.insert(pair <string , int>  ("DK" , 11));
-	cards.insert(pair <string , int>  ("DA" , 12));
-	// Club
-	for(int i=2;i<=9;i++)
-		cards.insert(pair <string , int>  ("C"+to_string(2) , 13+i-2));
-	cards.insert(pair <string , int>  ("CT"  , 13+8));
-	cards.insert(pair <string , int>  ("CJ" , 13+9));
-	cards.insert(pair <string , int>  ("CQ" , 13+10));
-	cards.insert(pair <string , int>  ("CK" , 13+11));
-	cards.insert(pair <string , int>  ("CA" , 13+12));
-	// Heart
-	for(int i=2;i<=9;i++)
-		cards.insert(pair <string , int>  ("H"+to_string(2) , 26+i-2));
-	cards.insert(pair <string , int>  ("HT"  , 26+8));
-	cards.insert(pair <string , int>  ("HJ" , 26+9));
-	cards.insert(pair <string , int>  ("HQ" , 26+10));
-	cards.insert(pair <string , int>  ("HK" , 26+11));
-	cards.insert(pair <string , int>  ("HA" , 26+12));
-	// Spades
-	for(int i=2;i<=9;i++)
-		cards.insert(pair <string , int>  ("S"+to_string(2) , 39+i-2));
-	cards.insert(pair <string , int>  ("ST"  , 39+8));
-	cards.insert(pair <string , int>  ("SJ" , 39+9));
-	cards.insert(pair <string , int>  ("SQ" , 39+10));
-	cards.insert(pair <string , int>  ("SK" , 39+11));
-	cards.insert(pair <string , int>  ("SA" , 39+12));
-	
-	print_map(cards);
+int main(void) {
+  int tmp;
+  int cover;
+  string card;
 
-	vector<string> dealer_deck;
-	vector<string> non_dealer_deck;
-	vector<string> ground;
-	string card;
-	int winner;
-	bool dealer_turn = false;
-	//getline (cin, card);
-	while(1) {
-		cin >> card;
-		if(card == "#") break;
-		if(dealer_turn == true) {
-			dealer_deck.push_back(card);
-		} else {
-			non_dealer_deck.push_back(card);
-		}
-		dealer_turn = !dealer_turn;
-	}
+  deque<int> player, dealer, middle;
+  bool p = true;  // player starts the game, not the dealer
+  cover = 0;
 
-	while(1) {
-		if(dealer_deck.size() == 0) {winner = 1; break;}
-		if(non_dealer_deck.size() == 0) {winner = 2 ; break; }
-		dealer_turn = false;
-		if(dealer_turn) {
-			ground.push_back( dealer_deck.back());
-			dealer_deck.pop_back();
-		}	
-		else {
-			ground.push_back( non_dealer_deck.back());
-			non_dealer_deck.pop_back();
-		}
-	}
-	
+  for (int i = 1;; i++) {
+    cin >> card;
+    if (card == "#") break;
+    if (i % 2 == 0) {
+      dealer.push_front(card2int(card));
+    } else {
+      player.push_front(card2int(card));
+    }
+  }
+
+  while (true) {
+    if (player.empty() && p || dealer.empty() && !p) break;
+
+    // cout << "player = " << player.size() << " dealer = " << dealer.size() <<
+    // " cover = " << cover << endl; cout << "middle = " << middle.size() << "
+    // front = " << middle.front() << endl;
+    if (p) {
+      tmp = player.front();
+      player.pop_front();
+      middle.push_front(tmp);
+
+      if (tmp > 10) {
+        p = false;
+        cover = tmp - 10;
+        continue;
+      }
+
+      if (cover > 0) {
+        if (cover == 1) {
+          cover = 0;
+          while (!middle.empty()) {
+            dealer.push_back(middle.back());
+            middle.pop_back();
+          }
+          p = false;
+        } else {
+          cover--;
+        }
+      } else {
+        p = false;
+      }
+    } else {
+      tmp = dealer.front();
+      dealer.pop_front();
+      middle.push_front(tmp);
+
+      if (tmp > 10) {
+        p = true;
+        cover = tmp - 10;
+        continue;
+      }
+
+      if (cover > 0) {
+        if (cover == 1) {
+          cover = 0;
+          while (!middle.empty()) {
+            player.push_back(middle.back());
+            middle.pop_back();
+          }
+          p = true;
+        } else {
+          cover--;
+        }
+      } else {
+        p = true;
+      }
+    }
+  }
+
+  if (player.size() == 0) {
+    printf("%d%3d\n", 1, dealer.size());
+  } else {
+    printf("%d%3d\n", 2, player.size());
+  }
+
+  return 0;
 }
